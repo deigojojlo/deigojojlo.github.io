0c1830554e12fcf0573fc38baabe2e331f6b8fc4
valentin mouchès <valentin.mouches@gmail.com>
2023-07-02 22:22:11 +0200

    chrono

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const timerElement = document.getElementById("chrono")
-let diff = 132000
+let diff = 128340
 
 function time () {
     diff <= 0 ? clearInterval(a) : diff - 1
     let heures = parseInt(diff/3600,10)
     let minutes = parseInt((diff%3600)/60,10)
     let seconds = parseInt((diff%3600)%60,10)
-    timerElement.innerText = '${heures}:${minutes}:${seconds}'
+    timerElement.innerText = heures + ":" + minutes + ":" + seconds
     
 }
 
